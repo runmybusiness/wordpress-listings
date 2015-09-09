0cdb72a4bc8c4d8b02046f4fa6b9640c762c225a
Tom Schlick <tom@runmybusiness.com>
2015-09-09 12:36:38 -0400

    version bump

--- run-my-business.php
+++ run-my-business.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: RunMyBusiness
 Plugin URI: https://wordpress.org/plugins/runmybusiness-listings/
-Version: 1.0.8
+Version: 1.0.9
 Description: This plugin imports data from RunMyBusiness.
 */
 
