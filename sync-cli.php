31c486a52fce05734706a3390212c9d555a5e04f
Tom Schlick <tom@tomschlick.com>
2015-10-06 03:40:01 -0400

    Update sync-cli.php

--- sync-cli.php
+++ sync-cli.php
@@ -1,25 +1,26 @@
 <?php
-    if (php_sapi_name() !== 'cli') {
-        die("Meant to be run from command line");
-    }
 
-    function find_wordpress_base_path()
-    {
-        $dir = dirname(__FILE__);
-        do {
-            //it is possible to check for other files here
-            if (file_exists($dir."/wp-config.php")) {
-                return $dir;
-            }
-        } while ($dir = realpath("$dir/.."));
-        return null;
-    }
+if (php_sapi_name() !== 'cli') {
+    die("Meant to be run from command line");
+}
 
-    define('BASE_PATH', find_wordpress_base_path()."/");
-    define('WP_USE_THEMES', false);
-    global $wp, $wp_query, $wp_the_query, $wp_rewrite, $wp_did_header;
-    require(BASE_PATH . 'wp-load.php');
+function find_wordpress_base_path()
+{
+    $dir = dirname(__FILE__);
+    do {
+        //it is possible to check for other files here
+        if (file_exists($dir."/wp-config.php")) {
+            return $dir;
+        }
+    } while ($dir = realpath("$dir/.."));
+    return null;
+}
 
-    include('includes/sync-all.php');
+define('BASE_PATH', find_wordpress_base_path()."/");
+define('WP_USE_THEMES', false);
+global $wp, $wp_query, $wp_the_query, $wp_rewrite, $wp_did_header;
+require(BASE_PATH . 'wp-load.php');
 
-    runmybusiness_do_update_content();
+include('includes/sync-all.php');
+
+runmybusiness_do_update_content();
