bde49c4f1d9bc034e90301aaf5fca89e1edc409b
Tom Schlick <tom@tomschlick.com>
2015-10-06 03:23:03 -0400

    Create sync-cli.php

---
+++ sync-cli.php
@@ -0,0 +1,24 @@
+<?php
+    if( php_sapi_name() !== 'cli' ) {
+        die("Meant to be run from command line");
+    }
+
+    function find_wordpress_base_path() {
+        $dir = dirname(__FILE__);
+        do {
+            //it is possible to check for other files here
+            if( file_exists($dir."/wp-config.php") ) {
+                return $dir;
+            }
+        } while( $dir = realpath("$dir/..") );
+        return null;
+    }
+
+    define( 'BASE_PATH', find_wordpress_base_path()."/" );
+    define('WP_USE_THEMES', false);
+    global $wp, $wp_query, $wp_the_query, $wp_rewrite, $wp_did_header;
+    require(BASE_PATH . 'wp-load.php');
+
+    include('includes/sync_all.php');
+
+    runmybusiness_do_update_content();
