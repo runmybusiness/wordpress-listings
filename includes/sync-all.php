de719e58b0f96a5fc2e2c5962eaee7015d36076e
Tom Schlick <tom@tomschlick.com>
2015-10-06 03:27:24 -0400

    Update sync-all.php

--- includes/sync-all.php
+++ includes/sync-all.php
@@ -1,33 +1,36 @@
 <?php
 
-// Register new action to run on cron execution
-add_action('runmybusiness_update_content', 'runmybusiness_do_update_content');
-function runmybusiness_do_update_content()
-{
-    global $wpdb;
-    global $user_ID;
-    $runmybusiness_options = get_option('runmybusiness_options');
 
-    $context = stream_context_create([
-        'http' => [
-            'header' => 'Authorization: Basic ' . base64_encode($runmybusiness_options['runmybusiness_username'] . ':' . $runmybusiness_options['runmybusiness_password']),
-        ],
-    ]);
+if( ! function_exists('runmybusiness_do_update_content')) {
+    // Register new action to run on cron execution
+    add_action('runmybusiness_update_content', 'runmybusiness_do_update_content');
+    function runmybusiness_do_update_content()
+    {
+        global $wpdb;
+        global $user_ID;
+        $runmybusiness_options = get_option('runmybusiness_options');
 
-    $runmybusiness_listing_url = isset($runmybusiness_options['runmybusiness_listing_url']) ? $runmybusiness_options['runmybusiness_listing_url'] : '';
-    if ($runmybusiness_listing_url) {
-        require_once plugin_dir_path(__FILE__) . '/sync-listings.php';
-    }
+        $context = stream_context_create([
+            'http' => [
+                'header' => 'Authorization: Basic ' . base64_encode($runmybusiness_options['runmybusiness_username'] . ':' . $runmybusiness_options['runmybusiness_password']),
+            ],
+        ]);
 
-    $runmybusiness_people_url = isset($runmybusiness_options['runmybusiness_people_url']) ? $runmybusiness_options['runmybusiness_people_url'] : '';
-    if ($runmybusiness_people_url) {
-        require_once plugin_dir_path(__FILE__) . '/sync-people.php';
-    }
+        $runmybusiness_listing_url = isset($runmybusiness_options['runmybusiness_listing_url']) ? $runmybusiness_options['runmybusiness_listing_url'] : '';
+        if ($runmybusiness_listing_url) {
+            require_once plugin_dir_path(__FILE__) . '/sync-listings.php';
+        }
 
-    $hooks_path = get_template_directory() . '/runmybusiness_hooks.php';
-    if (file_exists($hooks_path)) {
-        include($hooks_path);
+        $runmybusiness_people_url = isset($runmybusiness_options['runmybusiness_people_url']) ? $runmybusiness_options['runmybusiness_people_url'] : '';
+        if ($runmybusiness_people_url) {
+            require_once plugin_dir_path(__FILE__) . '/sync-people.php';
+        }
 
-        rmb_post_sync_hook();
+        $hooks_path = get_template_directory() . '/runmybusiness_hooks.php';
+        if (file_exists($hooks_path)) {
+            include($hooks_path);
+
+            rmb_post_sync_hook();
+        }
     }
 }
