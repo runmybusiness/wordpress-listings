38bd7b55da463ad2ffb863c6063e3de6e783f6d6
Tom Schlick <tom@runmybusiness.com>
2016-03-07 01:03:44 -0500

    check if functions exist before declaring them

--- functions.php
+++ functions.php
@@ -2,48 +2,57 @@
 
 ini_set('xdebug.var_display_max_depth', 50);
 
-function array_get($array, $key, $default = null)
-{
-    if (is_null($key)) {
+if (!function_exists('array_get')) {
+    function array_get($array, $key, $default = null)
+    {
+        if (is_null($key)) {
+            return $array;
+        }
+        if (isset($array[$key])) {
+            return $array[$key];
+        }
+        foreach (explode('.', $key) as $segment) {
+            if (!is_array($array) || !array_key_exists($segment, $array)) {
+                return $default;
+            }
+            $array = $array[$segment];
+        }
+
         return $array;
     }
-    if (isset($array[$key])) {
-        return $array[$key];
-    }
-    foreach (explode('.', $key) as $segment) {
-        if (!is_array($array) || !array_key_exists($segment, $array)) {
-            return $default;
-        }
-        $array = $array[$segment];
-    }
-
-    return $array;
 }
 
-function getField(array $dataArray, $field_name, $fallback = null)
-{
-    foreach ($dataArray['fields']['data'] as $row) {
-        if ($row['field']['legacy_id'] == $field_name) {
-            return $row['markup'];
+if (!function_exists('getField')) {
+    function getField(array $dataArray, $field_name, $fallback = null)
+    {
+        foreach ($dataArray['fields']['data'] as $row) {
+            if ($row['field']['legacy_id'] == $field_name) {
+                return $row['markup'];
+            }
         }
-    }
 
-    return $fallback;
+        return $fallback;
+    }
 }
 
-function getFieldArray($dataArray, $field_name, $fallback = null)
-{
-    foreach ($dataArray['fields']['data'] as $row) {
-        if ($row['field']['legacy_id'] == $field_name) {
-            return $row;
+if (!function_exists('getFieldArray')) {
+    function getFieldArray($dataArray, $field_name, $fallback = null)
+    {
+        foreach ($dataArray['fields']['data'] as $row) {
+            if ($row['field']['legacy_id'] == $field_name) {
+                return $row;
+            }
         }
-    }
 
-    return $fallback;
+        return $fallback;
+    }
 }
 
-function array_get_from_field_array(array $dataArray, $field_name, $key, $default = null)
-{
-    $array = getFieldArray($dataArray, $field_name);
-    return array_get($array, $key, $default);
+if (!function_exists('array_get_from_field_array')) {
+    function array_get_from_field_array(array $dataArray, $field_name, $key, $default = null)
+    {
+        $array = getFieldArray($dataArray, $field_name);
+
+        return array_get($array, $key, $default);
+    }
 }
