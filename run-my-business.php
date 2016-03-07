b7fa6d2d8be547867c9e3e7828762a0be44e2a7d
Tom Schlick <tom@runmybusiness.com>
2016-03-07 01:04:30 -0500

    tag 1.0.18

--- run-my-business.php
+++ run-my-business.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: RunMyBusiness
 Plugin URI: https://wordpress.org/plugins/runmybusiness-listings/
-Version: 1.0.17
+Version: 1.0.18
 Description: This plugin imports data from RunMyBusiness.
 */
 
