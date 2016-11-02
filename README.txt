55d719503c8faeb6e037a3db26b4774b100eedd4
Tom Schlick <tom@tomschlick.com>
2016-11-02 16:55:49 -0400

    1.0.40

--- README.txt
+++ README.txt
@@ -1,6 +1,6 @@
 === RunMyBusiness ===
 Contributors: (runmybusiness)
-Stable tag: 1.0.38
+Stable tag: 1.0.40
 Tags: realestate, properties, listings, runmybusiness
 Requires at least: 3.0.1
 Tested up to: 4.6
--- run-my-business.php
+++ run-my-business.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: RunMyBusiness
 Plugin URI: https://wordpress.org/plugins/runmybusiness-listings/
-Version: 1.0.38
+Version: 1.0.40
 Description: This plugin imports data from RunMyBusiness.
 */
 
