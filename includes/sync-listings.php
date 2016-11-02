a2697fbf3703dbe191017a3281a5f3d5566f1bc6
Tom Schlick <tom@tomschlick.com>
2016-11-02 02:27:27 -0400

    use short name

--- includes/sync-listings.php
+++ includes/sync-listings.php
@@ -49,7 +49,7 @@ if (! empty($runmybusiness_data->data)) {
             $postMetaFunction($post_id, 'location', $item->property->address->geolookup->street->full);
         }
         if (! empty($item->property->address->geolookup->province->name)) {
-            $postMetaFunction($post_id, 'state', $item->property->address->geolookup->province->name);
+            $postMetaFunction($post_id, 'state', $item->property->address->geolookup->province->short_name);
         }
         if (! empty($item->property->building_size)) {
             $postMetaFunction($post_id, 'square_footage', $item->property->building_size);
