fe755da64fd25ee1b6a7fd78d70cfc0d292ae6c5
Tom Schlick <tom@tomschlick.com>
2016-12-09 16:44:24 -0500

    fixes

--- includes/shortcode-rates-table.php
+++ includes/shortcode-rates-table.php
@@ -11,39 +11,41 @@ function rmb_signup_show_form($attr = [])
 {
     $rates = @json_decode(file_get_contents(plugin_dir_path(__FILE__) . '../rates/rates-cache.json'), true);
 
-    $form = <<<HTMLPLUGIN
-<div class="rmb-rate-index">
-	<div class="rmb-rate-table">
-		<div class="rmb-header">
-			<div class="rmb-date">
-			    <?php echo date('F j, Y'); ?>
-			</div>
-			<div class="rmb-updated">Last Updated: August 10, 2016 - 10:30 EDT</div>
-		</div>
-		<div class="rmb-rate-block">
-		    <?php foreach($rates as $row): ?>
-		    <?php $class = ($row['pct'][0] == '-') ? 'rmb-neg' : 'rmb-pos'; ?>
-			<div class="rmb-rate">
-				<div class="rmb-rate-name">
-				    <?php echo $row['title']; ?>
-				</div>
-				<div class="rmb-rate-value <?php echo $class ?>">
-				    <?php echo $row['value']; ?>
-				</div>
-				<div class="rmb-rate-change <?php echo $class ?>">
-				    <?php echo $row['pct']; ?>
-				</div>
-			</div>
-			<?php endforeach; ?>
-		</div>
-	</div>
-//	<div class="rmb-rate-graph">
-//		<div class="rmb-graph">
-//			<!--graph goes here-->
-//		</div>
-//	</div>
-</div>
-HTMLPLUGIN;
+    $table = '
+        <div class="rmb-rate-index">
+            <div class="rmb-rate-table">
+                <div class="rmb-header">
+                    <div class="rmb-date">
+                        ' . date('F j, Y') . '
+                    </div>
+                    <div class="rmb-updated">Last Updated: August 10, 2016 - 10:30 EDT</div>
+                </div>
+                <div class="rmb-rate-block">
+                ';
+                    foreach($rates as $row):
+                    $class = ($row['pct'][0] == '-') ? 'rmb-neg' : 'rmb-pos';
 
-    echo $form;
+                $table += '
+                    <div class="rmb-rate">
+                        <div class="rmb-rate-name">
+                            ' . $row['title'] . '
+                        </div>
+                        <div class="rmb-rate-value ' . $class . '">
+                            ' . $row["value"] . '
+                        </div>
+                        <div class="rmb-rate-change ' . $class . '">
+                            ' . $row["pct"] . '
+                        </div>
+                    </div>';
+
+                    endforeach;
+                $table += '</div>
+            </div>
+<!--        	<div class="rmb-rate-graph">-->
+<!--        		<div class="rmb-graph">-->
+<!--        		</div>-->
+<!--        	</div>-->
+        </div>';
+
+    echo $table;
 }
