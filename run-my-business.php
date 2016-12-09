68038f219430ebb8c4b3c2b2041a2cbc6118349b
Tom Schlick <tom@tomschlick.com>
2016-12-09 16:44:36 -0500

    version

--- run-my-business.php
+++ run-my-business.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: RunMyBusiness
 Plugin URI: https://wordpress.org/plugins/runmybusiness-listings/
-Version: 1.0.50
+Version: 1.0.51
 Description: This plugin imports data from RunMyBusiness.
 */
 
