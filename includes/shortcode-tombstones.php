e00b1231c09ce95476bc9e73bf91b9a52faf3db9
Tom Schlick <tom@tomschlick.com>
2017-01-10 15:23:36 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- includes/shortcode-tombstones.php
+++ includes/shortcode-tombstones.php
@@ -30,25 +30,25 @@ HTMLBLOCK;
     if ($query->have_posts()) {
         // Start the Loop.
         while ($query->have_posts()) : $query->the_post();
-            global $post;
-            $rmb_post_custom = json_decode(get_post_meta($post->ID, 'runmybusiness_datastring')[0], true);
+        global $post;
+        $rmb_post_custom = json_decode(get_post_meta($post->ID, 'runmybusiness_datastring')[0], true);
 
-            $id = array_get($rmb_post_custom, 'id');
-            $title = array_get($rmb_post_custom, 'property.name');
-            $img = array_get($rmb_post_custom, 'property.primaryPhoto.sizes.600_sq');
-            $img_is_placeholder = array_get($rmb_post_custom, 'property.primaryPhoto.placeholder', false)  ? ' placeholder' : '';
-            $location = implode(', ', [
+        $id = array_get($rmb_post_custom, 'id');
+        $title = array_get($rmb_post_custom, 'property.name');
+        $img = array_get($rmb_post_custom, 'property.primaryPhoto.sizes.600_sq');
+        $img_is_placeholder = array_get($rmb_post_custom, 'property.primaryPhoto.placeholder', false)  ? ' placeholder' : '';
+        $location = implode(', ', [
                 array_get($rmb_post_custom, 'property.address.geolookup.city'),
                 array_get($rmb_post_custom, 'property.address.geolookup.province.short_name'),
             ]);
-            $loan_closing_date = date('M j, Y', strtotime(array_get($rmb_post_custom, 'loan.closing_date.date', '')));
-            $loan_amount = array_get($rmb_post_custom, 'loan.amount.formatted');
-            $loan_type = array_get($rmb_post_custom, 'loan.type.name');
-            $loan_term = array_get($rmb_post_custom, 'loan.term');
-            $loan_term = $loan_term ? $loan_term . ' Months' : '';
-            $loan_to_value = array_get($rmb_post_custom, 'loan.loan_to_value');
+        $loan_closing_date = date('M j, Y', strtotime(array_get($rmb_post_custom, 'loan.closing_date.date', '')));
+        $loan_amount = array_get($rmb_post_custom, 'loan.amount.formatted');
+        $loan_type = array_get($rmb_post_custom, 'loan.type.name');
+        $loan_term = array_get($rmb_post_custom, 'loan.term');
+        $loan_term = $loan_term ? $loan_term . ' Months' : '';
+        $loan_to_value = array_get($rmb_post_custom, 'loan.loan_to_value');
 
-            $table .= <<<HTMLBLOCK
+        $table .= <<<HTMLBLOCK
 <div class="rmb-tombstone-block" data-rmb-id="tombstone-{$id}">
     <div class="rmb-tombstone-image{$img_is_placeholder}">
         <img src="{$img}">
